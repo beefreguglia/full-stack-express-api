4bf2b139fad84768da5c2391446a7b63be836650
Bernardo <beefreguglia@gmail.com>
2024-09-02 08:55:50 -0300

    feature: using route params

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ const PORT = 3333
 
 const app = express()
 
-app.get("/", (request, response) => {
-  console.log("Request:", request, "Response:", response)
-  response.send("Hello World express")
+app.get("/products/:id/:category", (request, response) => {
+  const { id, category } = request.params
+
+  response.send(`Produto ${id} e categoria ${category}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
