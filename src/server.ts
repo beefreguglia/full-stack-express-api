7c3fa0fc0b9c6c95be950cd05daba95ea3ac39e3
Bernardo <beefreguglia@gmail.com>
2024-09-02 09:24:35 -0300

    feature: response with status and in json

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,19 @@ const PORT = 3333
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/products", (request, response) => {
-  const { page, limit } = request.query
+  const { page, limit } = request.body
 
   response.send(`Page: ${page} Limit: ${limit}`)
+})
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body
+
+  // response.send(`Produto ${name} custa $ ${price}`)
+  response.status(201).json({ name, price })
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
