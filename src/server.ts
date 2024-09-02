56e75cce4f333de39c7917ea2e461da469b85d0f
Bernardo <beefreguglia@gmail.com>
2024-09-02 09:35:10 -0300

    feature: using middleware in local scope

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,9 @@ const app = express()
 
 app.use(express.json())
 
-app.use(myMiddleware)
+
+// Middleware global, aplica em todas rotas abaixo dele
+// app.use(myMiddleware)
 
 app.get("/products", (request, response) => {
   const { page, limit } = request.body
@@ -15,7 +17,9 @@ app.get("/products", (request, response) => {
   response.send(`Page: ${page} Limit: ${limit}`)
 })
 
-app.post("/products", (request, response) => {
+
+// Middleware local, aplica somente para essa rota
+app.post("/products", myMiddleware, (request, response) => {
   const { name, price } = request.body
 
   // response.send(`Produto ${name} custa $ ${price}`)
