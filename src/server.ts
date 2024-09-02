5b5a078b11bc2aaa18bdda1781af5d1e95015153
Bernardo <beefreguglia@gmail.com>
2024-09-02 08:52:18 -0300

    feature: create first route

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,10 @@ const PORT = 3333
 
 const app = express()
 
+app.get("/", (request, response) => {
+  console.log("Request:", request, "Response:", response)
+  response.send("Hello World express")
+})
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
